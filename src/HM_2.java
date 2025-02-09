b2e7ea20f9398dd9fb416d9633323c9462b08b92
Amantai918 <ajdama918@gmail.com>
2025-02-10 02:19:13 +0600

    Update HM_2.java

--- src/HM_2.java
+++ src/HM_2.java
@@ -2,13 +2,19 @@ import java.util.Random;
 
 public class HM_2 {
     public static void main(String[] args) {
-        System.out.println(permission(20, 15)); //можно
-        System.out.println(permission(25, 28)); //можно
-        System.out.println(permission(28, 27)); //можно
-        System.out.println(permission(55, -20)); //нельзя
-        System.out.println(permission(23, -25)); //нельзя
+        System.out.println(permission(25, 15));
+        //если возраст человека от 20 до 45 лет и температура на улице от -20 до 30 градусов можно гулять
+        System.out.println(permission(12, 25));
+        //если же человеку меньше 20 лет и температура на улице в диапазоне от 0 до 28 градусов можно гулять
+        System.out.println(permission(65, 15));
+        // если человеку больше 45 лет, то результат “Можно идти гулять” формируется только тогда когда на улице температура в диапазоне от -10 до 25 градусов
+        System.out.println(permission(46, 26));
+        //нельзя
+        System.out.println(permission(23, -25));
+        //нельзя
 
         System.out.println(permission(generateRandomAge(), 20));
+        // Всегда выдаёт "можно идти гулять" нашел решение в добавлении рандомной темпереатуры во втором примере
     }
 
     public static String permission(int humanAge, int outsideTemperature) {
